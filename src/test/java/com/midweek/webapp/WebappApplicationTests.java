c64779e2c1b1d6cdee2c52c3bc302b5d6297608c
padjei <oheneba1986@yahoo.com>
2021-06-23 03:42:28 -0500

    first commit

---
+++ src/test/java/com/midweek/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midweek.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
